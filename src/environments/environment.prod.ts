4c38433bc3a1306278977f4cce4896c68cd78a15
Eder Silva Dev <eder.dasilva2014@gmail.com>
2022-08-29 23:02:38 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // apiURL: 'https://git.heroku.com/ford-api.git:8080',
-  //apiURL: 'http://ford-api.herokuapp.com/:13915',
+  apiURL: 'http://ford-api.herokuapp.com',
 };
